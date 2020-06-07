e91c9ef260200273d93bdc936593ccea702079cc
eunji2368 <qkrdmswl2368@naver.com>
2020-06-07 16:52:41 +0900

    Create pascal triangle

---
+++ R/pascal.R
@@ -0,0 +1,18 @@
+#' Computes pascal triangle
+#'
+#' This function computes pascal triangles.
+#'
+#' @examples
+#'
+#' pascal triangle(5)
+pascalTriangle <- function(h) {
+  for(i in 0:(h-1)) {
+    s <- ""
+    for(k in 0:(h-i)) s <- paste(s, "  ", sep = "")
+    for(j in 0:i) {
+      s <- paste(s, sprintf("%3d", choose(i,j)), sep = "")
+    }
+    cat(s)
+    cat("\n")
+  }
+}
